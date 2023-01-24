9f94bf34c6f8e2dd47490ae673f6096d9000a207
Paulo Sergio <pauloserg861@gmail.com>
2023-01-24 20:51:00 -0300

    instalando bibliotecas e configurando servidor

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app')
 
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 3010;
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
 require('dotenv').config();
 const express = require('express');
+const logger = require('morgan');
+const db = require('./db');
 const app = express();
+//router imports
+const storeRouter = require('./routes/store');
+
+//database connection
+db.dbConnection();
+
+//watching logs requests
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+//setting cors
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "*");
+	res.setHeader("Access-Control-Allow-Headers", "*");
+	res.setHeader("Content-Type", "multipart/form-data");
+
+	next();
+})
+
+//routes
+app.use(storeRouter);
 
 module.exports = app;
