d15c7b6146a17d1e353983fd39476de8bfde67b0
Paulo Sergio <pauloserg861@gmail.com>
2023-01-25 10:31:52 -0300

    refatorando metodo de create customer

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -1,15 +1,27 @@
+const { httpStatusCode, throwNewError } = require("../../config/error-tratament");
 const CustomerModel = require("../models/customer.model");
 
-const createCustomer = async (req, res) => {
+exports.createCustomer = async (req, res) => {
 	const { ...data } = req.body;
 
-	const customer = await CustomerModel.create({
-		...data,
-	});
+	const isCustomer = await CustomerModel.findOne({ email: data.email });
+	console.log(isCustomer);
 
-	res.status(201).json({ customer, message: "cliente cadastrado com sucesso" });
+	try {
+		if(isCustomer) {
+			res.status(httpStatusCode.CONFLICT).json({ message: throwNewError.EXISTANT_REGISTER.message });
+		} else {
+			const customer = await CustomerModel.create({
+				...data,
+			});
+
+			res.status(201).json({ customer, message: "cliente cadastrado com sucesso" });
+		}
+	} catch (error) {
+		res.status(400).json({ error, message: 'não foi possível criar o cliente' })
+	}
 };
 
-module.exports = {
-	createCustomer,
-};
+exports.getCustomers = async (req, res) => {
+
+}
