22c96d52c8963b796dccf2ab973b6bc22813f062
Paulo Sergio <pauloserg861@gmail.com>
2023-01-25 21:40:39 -0300

    adicionando metodo de criacao de produto

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,15 +1,44 @@
 const {
 	httpStatusCode,
 	throwNewError,
+	successStatus,
 } = require("../../config/error-tratament");
 const ProductModel = require("../models/product.model");
+
+exports.createProduct = async () => {
+	const { ...data } = req.body;
+
+	const isProduct = await ProductModel.findOne({ idProduct: data.id_product });
+
+	try {
+		if (isProduct) {
+			res
+				.status(httpStatusCode.CONFLICT)
+				.json({ message: throwNewError.EXISTANT_REGISTER.message });
+		} else {
+			const product = await ProductModel.create({
+				...data,
+			});
+
+			res
+				.status(httpStatusCode.CREATED)
+				.json({ product, message: successStatus.CREATED.message });
+		}
+	} catch (error) {
+		res
+			.status(httpStatusCode.BAD_REQUEST)
+			.json({ error, message: throwNewError.BAD_REQUEST.message });
+	}
+};
 
 exports.getProducts = async (req, res) => {
 	const { id } = req.params;
 
 	const productId = id ? { _id: id } : null;
 
-	const product = await ProductModel.find(productId).sort({ created_at: 1 });
+	const product = await ProductModel.find(productId)
+		.hint("id_product_1")
+		.sort({ created_at: 1 });
 
 	try {
 		if (product) res.status(httpStatusCode.OK).json(product);
