2d3d33f7f73f645bc81640cf7ad358ffc448d6af
Paulo Sergio <pauloserg861@gmail.com>
2023-01-26 21:56:36 -0300

    implementando rota de geolocalizacao

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,7 +1,10 @@
 const router = require('./index');
-const { createStore, getStores } = require('../controllers/store.controller');
+const { createStore, getStores, getStoresByLocation } = require('../controllers/store.controller');
 
 router.post('/store/create', createStore);
 router.get('/stores/:id?', getStores);
+router.post('/stores/geolocation', getStoresByLocation);
 
 module.exports = router;
+
+// ex: http://localhost:1346?nome="+user+"&lat="+lat+"&lon="+lng, null
