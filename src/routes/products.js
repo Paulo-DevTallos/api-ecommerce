f6073bbc14fc1cc1bf98c71af67ad572494c444e
Paulo Sergio <pauloserg861@gmail.com>
2023-01-29 21:51:45 -0300

    implementando rota de update de produto

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,10 +5,12 @@ const {
 	removeProduct,
 	getProductByQueryParam,
 	filterProductsByStore,
+	updateProduct,
 } = require("../controllers/product.controller");
 
 router.post('/product/create', createProduct);
 router.get('/products/store/:id', filterProductsByStore);
+router.patch('/product/update/:id', updateProduct);
 router.get("/product/search", getProductByQueryParam);
 router.get("/products/:id?", getProducts);
 router.delete('/product/:id', removeProduct);
