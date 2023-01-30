af9ac11b9d1d6776d820c9d2ff6750a1918fda94
Paulo Sergio <pauloserg861@gmail.com>
2023-01-30 14:45:00 -0300

    implementando login do employee

---
+++ src/authentications/employee.login.js
@@ -0,0 +1,73 @@
+const { httpStatusCode, throwNewError } = require('../../config/error-tratament');
+const { Encript } = require('../helpers/cripto');
+const EmployeeModel = require('../models/employee.model');
+const SessionModel = require('../models/session.model');
+const jwt = require('jsonwebtoken');
+
+exports.login = async (req, res) => {
+	const { employee_email, password } = req.body;
+
+	// fazer checkagem dos campos
+	if (!employee_email && !password) {
+		res
+			.status(httpStatusCode.UNAUTHORIZED)
+			.json({ message: throwNewError.ENTITY_FIELDS_EMPTY.message });
+		return;
+	}
+	if (!employee_email || !password) {
+		res
+			.status(httpStatusCode.UNAUTHORIZED)
+			.json({ message: throwNewError.ANY_ENTITY_EMPTY.message });
+		return;
+	}
+
+	// encontrar customer para entrar no try catch
+	const employee = await EmployeeModel.findOne({ employee_email });
+
+	try {
+		if (employee) {
+			const checkPassword = await Encript.ComparePassword(password, employee.password);
+
+			if (!checkPassword) {
+				res
+					.status(httpStatusCode.UNAUTHORIZED)
+					.json({ message: throwNewError.INVALID_PASSWORD.message });
+			} else {
+				const secret = process.env.SECRET_KEY;
+
+				const token = jwt.sign(
+					{
+						id: employee._id,
+						name: employee.employee_name,
+						email: employee.employee_email,
+					},
+					secret
+				);
+
+				//criando sessao no banco de dados
+				const session = {
+					userKey: employee.employee_email,
+					jwt: token,
+				}
+
+				if (session.userKey === employee_email) {
+					await SessionModel.deleteOne({ userKey: session.userKey }).exec()
+				}
+
+				await SessionModel.create(session);
+
+				res
+					.status(httpStatusCode.OK)
+					.json({
+						employee,
+						password: employee.password = undefined,
+						token,
+					});
+			}
+		}
+	} catch (error) {
+		res
+			.status(httpStatusCode.INTRNAL_SERVER_ERROR)
+			.json({ message: throwNewError.SEVERAL_INTERAL_SERVER_ERROR.message });
+	}
+}
--- src/routes/employee.js
+++ src/routes/employee.js
@@ -2,7 +2,10 @@ const router = require('./index');
 const {
 	createEmployee,
 } = require('../controllers/employee.controller');
+const { login } = require('../authentications/employee.login');
 
 router.post('/employee/create', createEmployee)
+
+router.post('/employee/auth', login);
 
 module.exports = router;
