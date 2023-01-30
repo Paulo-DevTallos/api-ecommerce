8f4a10ae7217fe59d8e56e4f9f2a005b1b8d3c98
Paulo Sergio <pauloserg861@gmail.com>
2023-01-29 21:38:13 -0300

    incluindo rota para filtro de produtos por loja

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,11 +4,11 @@ const {
 	createProduct,
 	removeProduct,
 	getProductByQueryParam,
-	filterStoreByProducts,
+	filterProductsByStore,
 } = require("../controllers/product.controller");
 
 router.post('/product/create', createProduct);
-router.post('/product/stores', filterStoreByProducts);
+router.get('/products/store/:id', filterProductsByStore);
 router.get("/product/search", getProductByQueryParam);
 router.get("/products/:id?", getProducts);
 router.delete('/product/:id', removeProduct);
