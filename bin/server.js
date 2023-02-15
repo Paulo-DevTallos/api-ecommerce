846ca1fbe6818eaad5039f49c6742bba5617a16d
Paulo Sergio <pauloserg861@gmail.com>
2023-02-15 20:54:06 -0300

    implementando http server

---
+++ bin/server.js
@@ -0,0 +1,24 @@
+const app = require("../src/app");
+const http = require("http");
+const { httpStatusCode, throwNewError } = require("../config/error-tratament");
+
+const port = process.env.PORT;
+
+// creating server
+const server = http.createServer(app);
+
+server.listen(port, () =>
+	console.log(`Server running on http://localhost:${port}`)
+);
+server.on("error", onError);
+
+// Event listener for HTTP server error
+function onError(error) {
+	if (error.syscall !== "listen") {
+		throw new Error(
+			error,
+			httpStatusCode.SEVERAL_INTERAL_SERVER_ERROR,
+			throwNewError.SEVERAL_INTERAL_SERVER_ERROR.message
+		);
+	}
+}
