c547f9fa8117b5cd99ce156938a63342935d277e
Paulo Sergio <pauloserg861@gmail.com>
2023-02-16 10:50:46 -0300

    setando bodyparser em config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-	development: {
-		database: process.env.DB_URL_STRING_CONNECTION,
-		set_timestamp: {
-			timestamp: true,
-		},
-		timezone: "-03:00",
+	bodyParserUrlEncoded: {
+		extended: true
 	},
 };
