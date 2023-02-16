72b4e4c9a801f05b640940591fc44ce5124826bb
Paulo Sergio <pauloserg861@gmail.com>
2023-02-16 20:51:52 -0300

    implementando rotas de ordenamento de produtos

--- src/routes/products.js
+++ src/routes/products.js
@@ -7,14 +7,16 @@ const {
 	filterProductsByStore,
 	updateProduct,
 	getPaginatedProducts,
-	ordenateProducts,
+	ordenateProductsByPrice,
+	ordenateProductsByCreation
 } = require("../controllers/product.controller");
 
 router.post("/product/create", createProduct);
 router.get("/products/store/:id", filterProductsByStore);
 router.patch("/product/update/:id", updateProduct);
-router.get("/product/search", getProductByQueryParam);
-router.get("/products/ordenate", ordenateProducts);
+router.get("/products/search", getProductByQueryParam);
+router.get("/products/price", ordenateProductsByPrice);
+router.get("/products/create", ordenateProductsByCreation);
 router.get("/products?page=:page&limit=:limit", getPaginatedProducts);
 router.get("/products/:id?", getProducts);
 router.delete("/product/:id", removeProduct);
