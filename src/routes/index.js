30ac620c638cfdcdaa7b0ec9cca70779db51a51b
Paulo Sergio <pauloserg861@gmail.com>
2023-02-16 11:00:45 -0300

    criando distribuidor de rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,20 @@
+const fs = require('fs');
+const validFileTypes = ['js'];
 const router = require('express').Router();
 
-module.exports = router;
+const requireFilesRouters = (routerDir, app) => {
+	fs.readdirSync(routerDir).forEach(fileName => {
+		const checkValidFileTypes = validFileTypes.indexOf(fileName.split('.').pop()) === -1
+
+		if (fileName === 'index.js' && routerDir === __dirname || checkValidFileTypes) {
+			return;
+		}
+
+		require(`${routerDir}/${fileName}`)(app)
+	})
+}
+
+
+module.exports = router, function(app) {
+	requireFilesRouters(__dirname, app)
+};
