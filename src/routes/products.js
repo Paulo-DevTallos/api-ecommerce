6c6877399c29db53370e2735b2027ed47a925924
Paulo Sergio <pauloserg861@gmail.com>
2023-02-17 13:58:20 -0300

    removendo rotas de query price + create

--- src/routes/products.js
+++ src/routes/products.js
@@ -7,17 +7,15 @@ const {
 	filterProductsByStore,
 	updateProduct,
 	getPaginatedProducts,
-	ordenateProductsByPrice,
-	ordenateProductsByCreation
 } = require("../controllers/product.controller");
 
 router.post("/product/create", createProduct);
 router.get("/products/store/:id", filterProductsByStore);
 router.patch("/product/update/:id", updateProduct);
 router.get("/products/search", getProductByQueryParam);
-router.get("/products/price", ordenateProductsByPrice);
-router.get("/products/create", ordenateProductsByCreation);
-router.get("/products?page=:page&limit=:limit", getPaginatedProducts);
+
+//http://localhost:3030/products/paginate?page=1&limit=10&sort=price,desc
+router.get("/products/paginate", getPaginatedProducts);
 router.get("/products/:id?", getProducts);
 router.delete("/product/:id", removeProduct);
 
