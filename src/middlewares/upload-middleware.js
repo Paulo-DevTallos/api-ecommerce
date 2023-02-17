6ce167d28c029f851a4a4e1ae620af867ea82b71
Paulo Sergio <pauloserg861@gmail.com>
2023-02-17 20:26:55 -0300

    criando middleware de upload

---
+++ src/middlewares/upload-middleware.js
@@ -0,0 +1,15 @@
+const multer = require('multer');
+const path = require('path');
+
+const upload = multer({
+	storage: multer.diskStorage({
+		destination(req, file, callback) {
+			callback(null, path.resolve(__dirname, '../..', 'uploads'));
+		},
+		filename(req, file, callback) {
+			callback(null, `${Date.now()}-${file.originalname}`);
+		},
+	})
+});
+
+module.exports = upload;
