c104bf17c0487fe3a2239ec4170ae4c9c2e557ce
Paulo Sergio <pauloserg861@gmail.com>
2023-02-17 22:11:35 -0300

    criando moddleware de upload

--- src/middlewares/upload-middleware.js
+++ src/middlewares/upload-middleware.js
@@ -4,7 +4,7 @@ const path = require('path');
 const upload = multer({
 	storage: multer.diskStorage({
 		destination(req, file, callback) {
-			callback(null, path.resolve(__dirname, '../..', 'uploads'));
+			callback(null, path.resolve(__dirname, "../..", "uploads"));
 		},
 		filename(req, file, callback) {
 			callback(null, `${Date.now()}-${file.originalname}`);
