6e21f3ec996b159e7ebf6af07ddccbb99c88b74d
Paulo Sergio <pauloserg861@gmail.com>
2023-02-27 20:50:47 -0300

    fix: implementando host de desenvolvimento

--- config/config.js
+++ config/config.js
@@ -4,4 +4,10 @@ module.exports = {
 	bodyParserUrlEncoded: {
 		extended: true
 	},
+	development: {
+		host_connection: process.env.DB_URL_STRING_CONNECTION,
+	},
+	production: {
+		host_prod_connection: '',
+	}
 };
