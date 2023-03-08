8e4c2a4401de678f5d1b29cd45c7fb980a2881af
Paulo Sergio <pauloserg861@gmail.com>
2023-03-08 15:23:27 -0300

    feat: criando rota de login

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -6,11 +6,13 @@ const {
 	getCustomerByQueryParam,
 	addProductToCart
 } = require("../controllers/customer.controller");
+const { login } = require("../authentications/customer.login");
 
 router.post("/customer/create", createCustomer);
 router.get("/customer/search", getCustomerByQueryParam);
 router.patch('/customer/:customer_id/addcart/:product_id', addProductToCart);
 router.get("/customer/:id?", getCustomers);
 router.delete("/customer/:id", removeCustomer);
+router.post('/customer/auth', login);
 
 module.exports = router;
