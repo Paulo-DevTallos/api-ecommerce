4dc701891137b7925494af919664255a675681de
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-03 15:57:10 -0300

    refactor: reestruturando dominios de login do funcionário

--- src/authentications/employee.login.js
+++ src/authentications/employee.login.js
@@ -1,47 +1,38 @@
 const { httpStatusCode, throwNewError } = require('../../config/error-tratament');
 const { Encript } = require('../helpers/cripto');
-const EmployeeModel = require('../models/employee.model');
+const EmployeeService = require('../services/employees.service');
 const SessionModel = require('../models/session.model');
 const jwt = require('jsonwebtoken');
 
 exports.login = async (req, res) => {
 	const { employee_email, password } = req.body;
 
-	// fazer checkagem dos campos
-	if (!employee_email && !password) {
-		res
-			.status(httpStatusCode.UNAUTHORIZED)
-			.json({ message: throwNewError.ENTITY_FIELDS_EMPTY.message });
-		return;
-	}
 	if (!employee_email || !password) {
-		res
-			.status(httpStatusCode.UNAUTHORIZED)
-			.json({ message: throwNewError.ANY_ENTITY_EMPTY.message });
-		return;
+		res.status(httpStatusCode.UNPROCESSABLE_ENTITY).json({
+			message: throwNewError.ENTITY_FIELDS_EMPTY.message
+		})
 	}
 
-	// encontrar customer para entrar no try catch
-	const employee = await EmployeeModel.findOne({ employee_email });
-
-	//tratar erro do email digitado sendo diferente do email existente
+	const employee = await EmployeeService.findOneEmployeeService({ employee_email });
 
 	try {
 		if (employee) {
 			const checkPassword = await Encript.ComparePassword(password, employee.password);
 
 			if (!checkPassword) {
-				res
-					.status(httpStatusCode.UNAUTHORIZED)
-					.json({ message: throwNewError.INVALID_PASSWORD.message });
+				res.status(httpStatusCode.UNAUTHORIZED).json({
+					message: throwNewError.INVALID_PASSWORD.message
+				});
+
 			} else {
 				const secret = process.env.SECRET_KEY;
 
 				const token = jwt.sign(
 					{
 						id: employee._id,
-						name: employee.employee_name,
-						email: employee.employee_email,
+						employee_name: employee.employee_name,
+						employee_email: employee.employee_email,
+						registration: employee.registration
 					},
 					secret
 				);
@@ -68,8 +59,9 @@ exports.login = async (req, res) => {
 			}
 		}
 	} catch (error) {
-		res
-			.status(httpStatusCode.INTRNAL_SERVER_ERROR)
-			.json({ message: throwNewError.SEVERAL_INTERAL_SERVER_ERROR.message });
+		res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
+			error,
+			message: throwNewError.INTERNAL_SERVER_ERROR.message
+		});
 	}
 }
