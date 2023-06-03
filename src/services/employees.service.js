361432eca3d18b5f00233f75e7677d47e5e48a9f
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-03 15:54:04 -0300

    feat: criando findOne de employees

--- src/services/employees.service.js
+++ src/services/employees.service.js
@@ -1,5 +1,9 @@
 const EmployeeModel = require('../models/employee.model');
 
+exports.findOneEmployeeService = (identifier) => {
+	return EmployeeModel.findOne(identifier)
+}
+
 exports.createEmployeeService = async (body) => {
 	return await EmployeeModel.create(body)
 };
