5f4f1fab571303ffa18418ef6c7d3839a794b078
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-03 17:37:06 -0300

    feat: criando rota de update

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,11 +1,15 @@
 const router = require('./index');
 const {
 	createEmployee,
+	updateEmployee
 } = require('../controllers/employee.controller');
 const { login } = require('../authentications/employee.login');
 
-router.post('/employee/create', createEmployee)
-
+//criação de novo funcionário
+router.post('/employee/create', createEmployee);
+//login
 router.post('/employee/auth', login);
+//atualizar um funcionário
+router.patch('/employee/:id', updateEmployee);
 
 module.exports = router;
