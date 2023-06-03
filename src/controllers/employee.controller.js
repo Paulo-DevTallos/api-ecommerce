aae50db717bc0c91e977155de25e73c7253061dd
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-03 17:36:40 -0300

    feat: implementando controlador de update de funcionarios

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -7,17 +7,18 @@ const { Encript } = require('../helpers/cripto');
 const EmployeeModel = require('../models/employee.model');
 const EmployeeService = require('../services/employees.service');
 const globalHelpers = require('../helpers/global');
+const { default: mongoose } = require('mongoose');
 
 exports.createEmployee = async (req, res) => {
 	const { ...data } = req.body;
-
+	// criar sistema de validação de senha
 	if (!data.employee_name || !data.employee_email || !data.password) {
 		res.status(httpStatusCode.UNPROCESSABLE_ENTITY).json({
 			message: throwNewError.ENTITY_FIELDS_EMPTY.message
 		})
 	}
 
-	const isEmployee = await EmployeeService.findOneEmployeeService({
+	const isEmployee = await EmployeeService.findEmployeeService({
 		employee_email: data.employee_email
 	});
 
@@ -50,9 +51,43 @@ exports.createEmployee = async (req, res) => {
 	}
 }
 
-exports.updateEmployee = (req, res) => {
+exports.updateEmployee = async (req, res) => {
 	const { id } = req.params;
 	const { ...data } = req.body;
 
-	console.log('teste')
+	//verificação a cerca de campos preenchidos
+	if (!data.employee_name && !data.employee_email && !data.password) {
+		res.status(httpStatusCode.UNPROCESSABLE_ENTITY).json({
+			message: throwNewError.EMPTY_FIELDS_FOR_UPDATE.message
+		})
+	}
+
+	//verificar se o id é valido
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		res.status(httpStatusCode.BAD_REQUEST).json({
+			message: throwNewError.IVALID_ID.message
+		})
+	}
+
+	const isEmployee = await EmployeeService.findEmployeeService({ _id: id })
+
+	try {
+		if (isEmployee) {
+			EmployeeService.updateEmployeeService(isEmployee._id, data)
+				.then(() => {
+
+					res.status(httpStatusCode.NO_CONTENT).json({
+						message: successStatus.UPDATED_RESOURCE.message
+					});
+				})
+		} else {
+			res.status(httpStatusCode.NOT_FOUND).json({
+				message: throwNewError.RESOURCE_NOT_FOUND.message
+			});
+		}
+	} catch (error) {
+		res.status(httpStatusCode.BAD_REQUEST).json({
+			error, message: throwNewError.REQUEST_FAILED.message
+		});
+	}
 }
