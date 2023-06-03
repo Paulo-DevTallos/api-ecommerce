09013281db5d172c5cc984787bec09fe8f214200
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-03 18:20:12 -0300

    style: commitando recursos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,3 @@
-const fs = require('fs');
-const validFileTypes = ['js'];
 const router = require('express').Router();
 
-const requireFilesRouters = (routerDir, app) => {
-	fs.readdirSync(routerDir).forEach(fileName => {
-		const checkValidFileTypes = validFileTypes.indexOf(fileName.split('.').pop()) === -1
-
-		if (fileName === 'index.js' && routerDir === __dirname || checkValidFileTypes) {
-			return;
-		}
-
-		require(`${routerDir}/${fileName}`)(app)
-	})
-}
-
-
-module.exports = router, function(app) {
-	requireFilesRouters(__dirname, app)
-};
+module.exports = router
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,7 +1,9 @@
 const { getOrders, createOrder } = require('../controllers/order.controller');
 const router = require('../routes/index');
 
+// get orders
 router.get('/orders/:id?', getOrders);
+// create order
 router.post('/order/create/:id', createOrder);
 
 module.exports = router;
