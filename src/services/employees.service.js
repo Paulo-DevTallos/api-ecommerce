3b8c0bbe03ce6cc94e8cd8bd8e12c9af88360717
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-03 17:36:15 -0300

    feat: implementando metodo de update de funcionarios e modos de busca

--- src/services/employees.service.js
+++ src/services/employees.service.js
@@ -1,9 +1,30 @@
 const EmployeeModel = require('../models/employee.model');
+const ObjectId = require('mongodb').ObjectId
 
-exports.findOneEmployeeService = (identifier) => {
-	return EmployeeModel.findOne(identifier)
-}
+exports.findEmployeeService = (identifier) => {
+
+	const isValidId = ObjectId.isValid(identifier)
+	// processa uma categoria de acordo com a busca solicitada
+	if (isValidId) {
+		return EmployeeModel.findById(identifier);
+	} else if (!identifier) {
+		return EmployeeModel.find();
+	}
+	else {
+		return EmployeeModel.findOne(identifier);
+	}
+};
 
 exports.createEmployeeService = async (body) => {
-	return await EmployeeModel.create(body)
+	return await EmployeeModel.create(body);
+};
+
+exports.updateEmployeeService = async (id, body) => {
+	return await EmployeeModel.updateOne(
+		{ _id: id },
+		{
+			$set: body
+		},
+		{ new: true }
+	);
 };
