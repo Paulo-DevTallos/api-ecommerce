a300b9d6aeecefbe6c3cff7fab6fc64ad2efb714
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-04 12:16:24 -0300

    feat: implementando status ao funcionario

--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -20,6 +20,11 @@ const schema = new mongoose.Schema({
 	registration: {
 		type: String
 	},
+	status: {
+		type: String,
+		enum: ['active', 'inactive'],
+		default: 'active'
+	},
 	created_at: {
 		type: Date,
 		default: Date.now
