01ee602b9072076134c7c9c7b51f3f63391c265e
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-17 10:54:51 -0300

    refactor: alterando estrutura de condicionais

--- src/authentications/employee.login.js
+++ src/authentications/employee.login.js
@@ -7,60 +7,60 @@ const jwt = require('jsonwebtoken');
 exports.login = async (req, res) => {
 	const { employee_email, password } = req.body;
 
-	if (!employee_email || !password) {
-		res.status(httpStatusCode.UNPROCESSABLE_ENTITY).json({
-			message: throwNewError.ENTITY_FIELDS_EMPTY.message
-		})
-	}
-
-	const employee = await EmployeeService.findEmployeeService({ employee_email });
-
 	try {
-		if (employee && employee.status !== 'inactive') {
-			const checkPassword = await Encript.ComparePassword(password, employee.password);
-
-			if (!checkPassword) {
-				res.status(httpStatusCode.UNAUTHORIZED).json({
-					message: throwNewError.INVALID_PASSWORD.message
-				});
-
-			} else {
-				const secret = process.env.SECRET_KEY;
-
-				const token = jwt.sign(
-					{
-						id: employee._id,
-						employee_name: employee.employee_name,
-						employee_email: employee.employee_email,
-						registration: employee.registration
-					},
-					secret
-				);
-
-				//criando sessao no banco de dados
-				const session = {
-					userKey: employee.employee_email,
-					jwt: token,
-				}
-
-				if (session.userKey === employee_email) {
-					await SessionModel.deleteOne({ userKey: session.userKey }).exec()
-				}
-
-				await SessionModel.create(session);
-
-				res
-					.status(httpStatusCode.OK)
-					.json({
-						employee,
-						password: employee.password = undefined,
-						token,
-					});
-			}
+		if (!employee_email || !password) {
+			res.status(httpStatusCode.UNPROCESSABLE_ENTITY).json({
+				message: throwNewError.ENTITY_FIELDS_EMPTY.message
+			})
 		} else {
-			res.status(httpStatusCode.UNAUTHORIZED).json({
-				message: throwNewError.ACCESS_DENIED.message
-			});
+			const employee = await EmployeeService.findEmployeeService({ employee_email });
+
+			if (employee && employee.status !== 'inactive') {
+				const checkPassword = await Encript.ComparePassword(password, employee.password);
+
+				if (!checkPassword) {
+					res.status(httpStatusCode.UNAUTHORIZED).json({
+						message: throwNewError.INVALID_PASSWORD.message
+					});
+
+				} else {
+					const secret = process.env.SECRET_KEY;
+
+					const token = jwt.sign(
+						{
+							id: employee._id,
+							employee_name: employee.employee_name,
+							employee_email: employee.employee_email,
+							registration: employee.registration
+						},
+						secret
+					);
+
+					//criando sessao no banco de dados
+					const session = {
+						userKey: employee.employee_email,
+						jwt: token,
+					}
+
+					if (session.userKey === employee_email) {
+						await SessionModel.deleteOne({ userKey: session.userKey }).exec()
+					}
+
+					await SessionModel.create(session);
+
+					res
+						.status(httpStatusCode.OK)
+						.json({
+							employee,
+							password: employee.password = undefined,
+							token,
+						});
+				}
+			} else {
+				res.status(httpStatusCode.UNAUTHORIZED).json({
+					message: throwNewError.ACCESS_DENIED.message
+				});
+			}
 		}
 	} catch (error) {
 		res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
